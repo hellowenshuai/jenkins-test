6c8ead850ba5e6557a36b15befcae2a4e3300c74
chenss <15893630801@163.com>
2019-12-23 22:52:18 +0800

    提交单例模式第一次

---
+++ src/main/com/design/Single.java
@@ -0,0 +1,22 @@
+package com.design;
+
+public class Single {
+    //1.私有化构造器
+    private Single(){}
+    //2定义一个类变量
+    public  static  Single instance;
+    //3.获取一个对象
+    public static Single getInstance() {
+        if (instance == null) {
+            instance = new Single();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Single instance = Single.getInstance();
+        Single instance2 = Single.getInstance();
+        System.out.println("instance:"+instance);
+        System.out.println("instance2:"+instance2);
+    }
+}
