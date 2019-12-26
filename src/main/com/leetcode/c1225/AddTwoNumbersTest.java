84255b6a555f4a118dd7a8a52cecb585b5973524
chenss <15893630801@163.com>
2019-12-26 21:24:06 +0800

    两数之和 链表形式 优化 由3ms提高到2ms

--- src/main/com/leetcode/c1225/AddTwoNumbersTest.java
+++ src/main/com/leetcode/c1225/AddTwoNumbersTest.java
@@ -32,59 +32,43 @@ public class AddTwoNumbersTest {
      * @return
      */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
-        }
-        // 默认为0
+        // 默认jinwei为0
         int jinwei = 0;
-        ListNode res = new ListNode(-1);
-        ListNode tmp = res;
-        while (l1 != null && l2 != null) {
-            int sum = l1.val + l2.val + jinwei;
+        ListNode temp = new ListNode(-1);
+        //保存结果中头指针
+        ListNode res = temp;
+        while (l1 != null || l2 != null) {
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            int sum = val1 + val2 + jinwei;
             //sum%10获取余数
-            res.next = new ListNode(sum % 10);
-            //sum%10获取整数
+            temp.next = new ListNode(sum % 10);
+            //sum%10获取进位的数
             jinwei = sum / 10;
-            //指针向后移动
-            res = res.next;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        while (l1 != null) {
-            int sum = l1.val + jinwei;
-            //sum%10获取整数
-            res.next = new ListNode(sum % 10);
-            jinwei = sum / 10;
-            //指针向后移动
-            res = res.next;
-            l1 = l1.next;
-        }
-        while (l2 != null) {
-            int sum = l2.val + jinwei;
-            //sum%10获取整数
-            res.next = new ListNode(sum % 10);
-            jinwei = sum / 10;
-            //指针向后移动
-            res = res.next;
-            l2 = l2.next;
+            if (l1 != null) {
+                //指针向后移动
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                //指针向后移动
+                l2 = l2.next;
+            }
+            temp = temp.next;
         }
         if (jinwei == 1) {
-            res.next = new ListNode(1);
+            temp.next = new ListNode(jinwei);
         }
         //res经过计算 ，已经在倒数第二个结点上，只有tmp还在res的头结点上
-        return tmp.next;
+        return res.next;
     }
 
 
     public static void main(String[] args) {
-        AddTwoNumbersTest test02 = new AddTwoNumbersTest ();
-        ListNode listNode = new ListNode(9);
+        AddTwoNumbersTest test02 = new AddTwoNumbersTest();
+        ListNode listNode = new ListNode(1);
         listNode.next = new ListNode(8);
 
-        ListNode listNode2 = new ListNode(1);
+        ListNode listNode2 = new ListNode(0);
         ListNode numbers = new ListNode(-1);
         try {
             numbers = test02.addTwoNumbers(listNode, listNode2);
