b5f1219daab194f311765a72e871639b5fe78b61
chenss <15893630801@163.com>
2019-12-26 23:47:55 +0800

    3. 无重复字符的最长子串 方法三：优化的滑动窗口

--- src/main/com/leetcode/c1225/LengthOfLongestSubstringTest.java
+++ src/main/com/leetcode/c1225/LengthOfLongestSubstringTest.java
@@ -1,8 +1,6 @@
 package com.leetcode.c1225;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 3. 无重复字符的最长子串
@@ -113,6 +111,44 @@ public class LengthOfLongestSubstringTest {
             }
         }
         //返回数组长度
+        return ans;
+    }
+
+    /**
+     * 上述的方法最多需要执行 2n 个步骤。事实上，它可以被进一步优化为仅需要 n 个步骤。我们可以定义字符到索引的映射，而不是使用集合来判断一个字符是否存在。 当我们找到重复的字符时，我们可以立即跳过该窗口。
+     *
+     * 也就是说，如果 s[j]s[j] 在 [i, j)[i,j) 范围内有与 j'j
+     * ′
+     *   重复的字符，我们不需要逐渐增加 ii 。 我们可以直接跳过 [i，j'][i，j
+     * ′
+     *  ] 范围内的所有元素，并将 ii 变为 j' + 1j
+     * ′
+     *  +1。
+     *
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetcod/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param s
+     * @return
+     */
+    public  int lengthOfLongestSubstring03(String s) {
+        int n = s.length(), ans = 0;
+        //创建map窗口,i为左区间，j为右区间，右边界移动
+        Map<Character, Integer> map = new HashMap<>();
+        for (int j = 0, i = 0; j < n; j++) {
+            // 如果窗口中包含当前字符，
+            if (map.containsKey(s.charAt(j))) {
+                //左边界移动到 相同字符的下一个位置和i当前位置中更靠右的位置，这样是为了防止i向左移动
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            //比对当前无重复字段长度和储存的长度，选最大值并替换
+            //j-i+1是因为此时i,j索引仍处于不重复的位置，j还没有向后移动，取的[i,j]长度
+            ans = Math.max(ans, j - i + 1);
+            // 将当前字符为key，下一个索引为value放入map中
+            // value为j+1是为了当出现重复字符时，i直接跳到上个相同字符的下一个位置，if中取值就不用+1了
+            map.put(s.charAt(j), j+1);
+        }
         return ans;
     }
 
